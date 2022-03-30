b4f64b3014161d792d2fd4184c82a717dab07fbe
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2022-03-30 20:22:02 +0200

    update pizza_order program

--- python/three/pizza_order_practie.py
+++ python/three/pizza_order_practie.py
@@ -2,25 +2,22 @@ print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M or L ")
 pepperoni = input("Do you want pepperoni? Y or N ")
 cheese = input("Do you want extra cheese? Y or N ")
-bill = 0
 
+bill = 0
 if size == "S":
     bill = 15
     if pepperoni == "Y":
         bill += 2
-    elif cheese == "Y":
-            bill += 1
 elif size == "M":
     bill = 20
-    if pepperoni == "Y":
-        bill += 3
-    elif cheese == "Y":
-            bill += 1
 else:
     bill = 25
-    if pepperoni == "Y":
-        bill += 3
-    elif cheese == "Y":
-            bill += 1
 
-print(f"Your final bill is: {bill}.")
+if pepperoni == "Y":
+    if size != "S":
+        bill += 3
+
+if cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is ${bill}.")
