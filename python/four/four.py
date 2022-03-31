450342bc0bae7be4d4be734e0942646dce008c1a
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2022-03-31 22:42:15 +0200

    course lists and random

--- python/four/four.py
+++ python/four/four.py
@@ -9,6 +9,3 @@ import random
 # In ra gia tri cua 1 bien trong 1 module khac
 # luu y: can import module do 
 # vd: print(my_module.today)
-
-number_random = random.randint(0, 5) + random.random()
-print(number_random)
---
+++ python/four/list.py
@@ -0,0 +1,14 @@
+list = [1, 2, 3]
+# list.append(5)
+# list.extend([1,4,5])
+# list.remove(3)
+# list.clear()
+# print(len(list))
+# list.pop(len(list) - 1)
+# list.pop(3)
+# list.reverse()
+
+# list2 = list.copy()
+# print(list2)
+
+
