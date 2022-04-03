031291861e4d6e629c3cfab5164d675378c9c318
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2022-04-03 16:38:39 +0200

    more for in range... exercises

--- python/five/five_course.py
+++ python/five/five_course.py
@@ -1,4 +1,13 @@
-fruits = ["Appele", "Peach", "Pear"]
-for fruit in fruits:
-    print(fruit)
-    print(fruit + " " + "Pie")
+# fruits = ["Appele", "Peach", "Pear"]
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + " " + "Pie")
+
+# for number in range(0, 9, 3):
+#     print(number)
+sum = 0
+for number in range(0, 101):
+    sum += number
+
+print(sum)
+
