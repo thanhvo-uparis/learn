7b0fd0cb526bf987b6f37d8a380c78897d62b105
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2022-04-09 19:34:42 +0200

    step 2: replace the Blanks in display list

--- python/seven/challenge1.py
+++ python/seven/challenge1.py
@@ -1,15 +1,18 @@
-#Step 1 
+#Step 2
 import random
 word_list = ["aardvark", "baboon", "camel"]
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess =input("Guess a letter: ").lower()
-print(chosen_word)
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-for letter in chosen_word:
-    if chosen_word[letter] == guess:
-        print("Right")
-    else:
-        print("Wrong")
+print(f"Pssstn the solution is {chosen_word}")
+
+display = []
+
+for n in chosen_word:
+    display.append('_')
+
+for position in range(len(chosen_word)):
+    if chosen_word[position] == guess:
+        display[position] = guess
+        
+print(display)
