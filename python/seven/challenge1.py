64c3366e488e9fadcf7255e6cb60e22a66c1fd74
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2022-04-09 19:04:25 +0200

    update challenge 1

--- python/seven/challenge1.py
+++ python/seven/challenge1.py
@@ -1,12 +1,15 @@
+#Step 1 
 import random
+word_list = ["aardvark", "baboon", "camel"]
 
-word_list = ["ardvark", "baboon", "camel"]
-list = random.choice(word_list)
-print(list)
-
-mot = input("Guess a letter: ")
-for n in range(0, len(list)):
-    if list[n] == mot:
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = random.choice(word_list)
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess =input("Guess a letter: ").lower()
+print(chosen_word)
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+for letter in chosen_word:
+    if chosen_word[letter] == guess:
         print("Right")
     else:
         print("Wrong")
