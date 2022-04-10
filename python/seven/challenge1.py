af9f8d9115f353a0a641415add289a00e28773e8
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2022-04-10 23:56:13 +0200

    add step3: use while to repeat player input unitl win

--- python/seven/challenge1.py
+++ python/seven/challenge1.py
@@ -1,18 +1,19 @@
-#Step 2
+#Step 3
 import random
+
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
-guess =input("Guess a letter: ").lower()
 print(f"Pssstn the solution is {chosen_word}")
 
 display = []
 
 for n in chosen_word:
     display.append('_')
-
-for position in range(len(chosen_word)):
-    if chosen_word[position] == guess:
-        display[position] = guess
-        
-print(display)
+while display.count('_') >= 1:
+    guess =input("Guess a letter: ").lower()
+    if display.count(guess) == 0:
+        for position in range(len(chosen_word)):
+            if chosen_word[position] == guess:
+                display[position] = guess
+                print(display)
