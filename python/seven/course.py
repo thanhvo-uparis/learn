3f5605151bbc270464ad7d00c13e16b51089a617
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2022-04-16 17:46:03 +0200

    update course if x in liste

---
+++ python/seven/course.py
@@ -0,0 +1,27 @@
+# display = []
+# display += "one two"
+# print(display)
+
+# list = ["Ha Noi", "Paris", "Tokyo"]
+# if "tokyo" in list:
+#     print("yes")
+# else:
+#     print("no")
+
+# list = ["Ha Noi", "Paris", "Tokyo"]
+# print(list.index("Tokyo"))
+
+
+# value = False
+# while not value:
+#     nb = int(input("Doan ngay sinh cua ban A: "))
+
+#     if nb == 15:
+#         value = True
+#         print("Ban da thua cuoc")
+
+myString = "hello"
+if "h" in myString:
+    print("yes")
+else:
+    print('no')
