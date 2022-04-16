6631e2255df66041cc1f6ee4b8cdd5585e95fbd3
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2022-04-16 16:26:45 +0200

    step 3 is complete

--- python/seven/challenge1.py
+++ python/seven/challenge1.py
@@ -3,23 +3,90 @@ import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 chosen_word = random.choice(word_list)
 print(f"Pssstn the solution is {chosen_word}")
 
 display = []
 
-for n in chosen_word:
-    display.append('_')
-while display.count('_') >= 1:
-    guess = input("Guess a letter: ").lower()
-    if chosen_word.count(guess) == 0:
-        print(f"{guess} khong dung, vui long nhap vao gia tri khac.")
-    elif display.count(guess) == 0:
-        for n in range(len(chosen_word)):
-            if chosen_word[n] == guess:
-                display[n] = guess
-        print(display)
-    else:
-        print(f"{guess} da ton tai.")
+for n in range(len(chosen_word)):
+    display.append("_")
 
-print("You win.")
+end_game = False
+while not end_game:
+  guess = input("Guess a letter: ")
+  if not display.count(guess) != 0:
+    if chosen_word.count(guess) != 0:
+      for n in range(len(chosen_word)):
+        if chosen_word[n] == guess:
+          display[n] = guess
+      print(display)
+    else:
+      print(f"{guess} khong chinh xac")
+  else:
+    print(f"{guess} da ton tai trong display")
+  
+  if "_" not in display:
+    end_game = True
+    print("You win!")
+
+#baboon
+# c : c khong trong babbon et c khong trong display
+# b: b trong baboon va b khong trong display
+# b: b da ton tai trong display
