9570e52be61a2841d14d2199ddbcd3b59f1a5ecf
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2022-04-21 21:50:32 +0200

    create course d9 and grading program

---
+++ python/d9/course_d9.py
@@ -0,0 +1,22 @@
+dictionary  = {
+'Bug': 'A moth in your computer.',
+'Log': 'This is test number 2'
+}
+# print(dictionary[0])
+
+#sua doi gia tri item
+# dictionary["Log"] = "This is test number 3"
+
+# for n in dictionary:
+#     #in ra key
+#     print(n)
+#     #in ra value cua key
+#     print(dictionary[n])
+
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"]},
+    "Germary": ["Berlin", "Humburg", "Stuttgart"]
+}
+
+# for n in travel_log:
+#     print(travel_log[0])
