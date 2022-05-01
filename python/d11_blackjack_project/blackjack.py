9bc235a55430153cd8d91f0776a9e169a85877eb
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2022-05-01 16:54:40 +0200

    update first step

--- python/d11_blackjack_project/blackjack.py
+++ python/d11_blackjack_project/blackjack.py
@@ -3,19 +3,19 @@ import random
 def blackjack():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10]
     times = 2
-    my_deck = []
+    my_cards = []
     for n in range(times):
-        my_deck.append(random.choice(cards))
+        my_cards.append(random.choice(cards))
 
-    my_total = sum(my_deck)
-    computer_deck = []
+    current_score = sum(my_cards)
+    computer_cards = []
     for n in range(times):
-        computer_deck.append(random.choice(cards))
+        computer_cards.append(random.choice(cards))
 
-    computer_total_score = sum(computer_deck)
+    computer_score = sum(computer_cards)
 
-    print(f"Your cards {my_deck}, current score: {my_total}")
-    print(f"Computer's first card: {computer_deck[0]}")
+    print(f"Your cards {my_cards}, current score: {current_score}")
+    print(f"Computer's first card: {computer_cards[0]}")
 
 blackjack()
 
