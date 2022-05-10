6cbc24f69118d4eeeb0d640423c38288ca0f71e7
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2022-05-10 23:13:24 +0200

    update hint 4-8

--- python/d11_blackjack_project/blackjack.py
+++ python/d11_blackjack_project/blackjack.py
@@ -1,21 +1,28 @@
 import random
 
-def blackjack():
-    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10]
-    user_card = []
-    computer_card = []
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
 
-    for n in range(2):
-        user_card.append(random.choice(cards))
-        computer_card.append(random.choice(cards))
-    return user_card, computer_card
+user_cards = []
+computer_cards = []
 
-def calculate_score(l1, l2):
-    if l1 == [10, 11] or l1 == [11,10]:
-        print("You win")
-    elif l2 == [10, 11] or l2 == [11, 10]:
-        print("Computer lose")
-    return sum(l1), sum(l2)
+for n in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+print(f"user cards: {user_cards}")
+print(f"computer cards: {computer_cards}")
+
+def calculate_score(cards):
+    if cards == [11, 10] or cards == [10, 11]:
+        return 0
+    if 11 is cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)
 
 # def blackjack():
 #     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10]
