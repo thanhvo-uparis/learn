73c485831c65345b9ea7ad5eb95a2eecbc68898b
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2022-05-14 22:40:52 +0200

    add hint 9 and 10

--- python/d11_blackjack_project/blackjack.py
+++ python/d11_blackjack_project/blackjack.py
@@ -7,16 +7,14 @@ def deal_card():
 
 user_cards = []
 computer_cards = []
+is_game_over = False
 
 for n in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
 
-print(f"user cards: {user_cards}")
-print(f"computer cards: {computer_cards}")
-
 def calculate_score(cards):
-    if cards == [11, 10] or cards == [10, 11]:
+    if 11 is cards and 10 is cards:
         return 0
     if 11 is cards and sum(cards) > 21:
         cards.remove(11)
@@ -24,38 +22,14 @@ def calculate_score(cards):
 
     return sum(cards)
 
-# def blackjack():
-#     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10]
-#     my_cards = []
-#     computer_cards = []
-#     times = 2
-#     for n in range(times):
-#         my_cards.append(random.choice(cards))
-#         computer_cards.append(random.choice(cards))
-#     current_score = sum(my_cards)
-#     computer_score = sum(computer_cards)
-#     verifier = True
-#     while verifier:
-#         print(f"Your cards {my_cards}, current score: {current_score}")
-#         print(f"Computer's first card: {computer_cards[0]}")
-#         choice = input("Type 'y' to get another card, type 'n' to pass: ")
-#         if current_score > 21:
-#             verifier = False
-#             print("Your lose!")
-#         elif choice == 'y':
-#             my_cards.append(random.choice(cards))
-#             computer_cards.append(random.choice(cards))
-#             blackjack()
-#         else:
-#             if current_score > computer_score:
-#                 print("You win!")
-#                 verifier = False
-#             elif current_score < computer_score:
-#                 print("You lose!")
-#                 verifier = False
-#             else: 
-#                 print("Hoa nhau!")
-#                 verifier = False
+print(f"Your cards: {user_cards}, current score: {calculate_score(user_cards)}")
+print(f"Computer's first card: {computer_cards}")
 
-# blackjack()
-
+if calculate_score(user_cards) == 0 or calculate_score(computer_cards) == 0 or calculate_score(user_cards) > 21:
+    is_game_over = True
+else:
+    check = input("Type 'y' to get another card, type 'n' to pass: ")
+    if check == 'y':
+        user_cards.append(deal_card)
+    else:
+        is_game_over = True
