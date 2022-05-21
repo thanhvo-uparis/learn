e5af76c298103e8805a65c7a9c326656701964dc
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2022-05-21 23:49:50 +0200

    course d12 scope

---
+++ python/d12_scope/course.py
@@ -0,0 +1,31 @@
+# number = 1
+
+# def inscrease_nb():
+#     number = 2
+#     print(f"Value of number: {number}")
+
+# inscrease_nb()
+# print(f"number est l'extérieur d'une fonction: {number}")
+
+# def test():
+#     title = "Bonjour à tous!"
+#     print(title)
+
+# test()
+# print(title)
+
+# game_level = 3
+# def create_enemy():
+#     enemies = ["Ha noi", "Paris", "Tokyo"]
+#     if game_level < 5:
+#         new_enemy = enemies[0]
+#     print(new_enemy)
+
+# create_enemy()
+# print(new_enemy)
+
+game_level = 3
+enemies = ["Ha noi", "Paris", "Tokyo"]
+if game_level < 5:
+    new_enemy = enemies[0]
+print(new_enemy)
---
+++ python/d12_scope/modify_global_variable.py
@@ -0,0 +1,8 @@
+age = 20
+
+def change():
+    print("Your age is added 5")
+    return age + 5
+
+age = change()
+print(age)
